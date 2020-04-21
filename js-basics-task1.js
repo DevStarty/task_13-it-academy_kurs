90c93c51a5b1b8b68cef10247aee0df4a492d13c
DevStarty <jskuza93@gmail.com>
2020-04-21 15:45:12 +0200

    it-academy-js-basics

--- js-basics-task1.js
+++ js-basics-task1.js
@@ -1,13 +1,13 @@
  let yourName = 'Jarosław Skuza'
  let birthDate = '24 08 1993'
-const num1 = 4.5;
-const num2 =4.5;
-const num3 =2;
+ const num1 = 4.5;
+ const num2 = 4.5;
+ const num3 = 2;
 
 
 
 
 
  console.log(`Hello! This program has been written by ${yourName}`);
-console.log(`Author has been born on ${birthDate}`);
-console.log(`The result of hidden equation is: ${(num1 + num2) * num3}`);
+ console.log(`Author has been born on ${birthDate}`);
+ console.log(`The result of hidden equation is: ${(num1 + num2) * num3}`);
